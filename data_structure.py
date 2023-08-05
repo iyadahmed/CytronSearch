58c6be38ec10a0e568357c38c529a39eaa0b1e42
Iyad Ahmed <iyadahmed430@gmail.com>
2023-08-05 21:24:02 +0300

    Rename PriorityQueue to Priority_Queue

--- data_structure.py
+++ data_structure.py
@@ -6,7 +6,7 @@ from heapq import heappush, heappop
 from requests_html import HTML
 
 
-class PriorityQueue:
+class Priority_Queue:
     def __init__(self):
         self._queue = []
 
@@ -54,7 +54,9 @@ class Processed_Search_Engine_Index:
         # where url_priority is the -1 * number of times the keyword appears in the website at url
         # we negated the priority so that we get the website with most occurrences of the keyword
         # when we pop from the priority queue (because Python's PriorityQueue is a min heap, we want a max heap)
-        self.keyword_index: defaultdict[str, PriorityQueue] = defaultdict(PriorityQueue)
+        self.keyword_index: defaultdict[str, Priority_Queue] = defaultdict(
+            Priority_Queue
+        )
 
         for keyword, keyword_count_map in raw_search_engine_index.keyword_index.items():
             for url_index, keyword_count in keyword_count_map.items():
